0fcb7303b4457dd0f562cfd943528cc21c8bbfd1
Avik Sargsyan <gukasyan.spa@gmail.com>
2021-03-01 11:52:19 +0400

    added "Shopping-list" parameters and styles

--- src/App/Components/Pages/ShoppingList/ShoppingList.jsx
+++ src/App/Components/Pages/ShoppingList/ShoppingList.jsx
@@ -6,9 +6,13 @@ const ShoppingList = () => {
   return (
     <div className="shopping-list__container">
       <h1>Shopping List</h1>
-      
+      <ul>
+        <li>Popular</li>
+        <li>Favorite</li>
+        <li>News</li>
+      </ul>
     </div>
-  ) 
-}
+  );
+};
 
 export default ShoppingList;
