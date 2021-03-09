1d7dc9fe019d134d491582701e58d544f9ea0dc1
Avik Sargsyan <gukasyan.spa@gmail.com>
2021-03-09 14:56:27 +0400

    added "navbar" icon styles.

--- src/App/Components/Navbar/Navbar.jsx
+++ src/App/Components/Navbar/Navbar.jsx
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [iconColorOfPage, setIconColorOfPage] = useState("/");
+  console.log(iconColorOfPage);
   
 
   return (
     <div className="navbar-container">
       <nav className="navbar-items">
-        <Link to="/">
+        <Link to="/" onClick={() => setIconColorOfPage("/")}>
           <i
+            style={{ 
+              color: iconColorOfPage === "/" ? "#8964D7" : "",
+              borderLeft: iconColorOfPage === "/" ? "3px solid #8964D7" : "",
+              paddingLeft: iconColorOfPage === "/" ? "48px" : ""
+            }}
             className="fas fa-th-list"
           ></i>
         </Link>
-        <Link to="/shopping-list" >
+        <Link
+          to="/shopping-list"
+          onClick={() => setIconColorOfPage("/shopping-list")}
+        >
           <i
+            style={{
+              color: iconColorOfPage === "/shopping-list" ? "#8964D7" : "",
+              borderLeft: iconColorOfPage === "/shopping-list" ? "3px solid #8964D7" : "",
+              paddingLeft: iconColorOfPage === "/shopping-list" ? "54px" : ""
+            }}
             className="fas fa-clipboard-list"
           ></i>
         </Link>
-        <Link to="/users" >
+        <Link to="/users" onClick={() => setIconColorOfPage("/users")}>
           <i
+            style={{
+              color: iconColorOfPage === "/users" ? "#8964D7" : "",
+              borderLeft: iconColorOfPage === "/users" ? "3px solid #8964D7" : "",
+              paddingLeft: iconColorOfPage === "/users" ? "45px" : ""
+            }}
             className="fas fa-users"
           ></i>
         </Link>
