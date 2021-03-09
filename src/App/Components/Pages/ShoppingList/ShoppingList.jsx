dc60e376d504c1bccd1e876be589b212502ebe0d
Avik Sargsyan <gukasyan.spa@gmail.com>
2021-03-09 11:42:12 +0400

    added the "shopping-list" and "users" headers".

--- src/App/Components/Pages/ShoppingList/ShoppingList.jsx
+++ src/App/Components/Pages/ShoppingList/ShoppingList.jsx
@@ -5,12 +5,20 @@ import "./ShoppingList.css";
 const ShoppingList = () => {
   return (
     <div className="shopping-list__container">
-      <h1>Shopping List</h1>
-      <ul>
-        <li><span>Popular</span></li>
-        <li><span>Favorite</span></li>
-        <li><span>News</span></li>
-      </ul>
+      <header>
+        <h1>Shopping List</h1>
+        <ul>
+          <li>
+            <span>Popular</span>
+          </li>
+          <li>
+            <span>Favorite</span>
+          </li>
+          <li>
+            <span>News</span>
+          </li>
+        </ul>
+      </header>
     </div>
   );
 };
--- src/App/Components/Pages/Users/Users.jsx
+++ src/App/Components/Pages/Users/Users.jsx
@@ -5,14 +5,22 @@ import "./Users.css";
 const Users = () => {
   return (
     <div className="users__container">
-      <h1>Users</h1>
-      <ul>
-        <li><span>Popular</span></li>
-        <li><span>Favorite</span></li>
-        <li><span>News</span></li>
-      </ul>
+      <header>
+        <h1>Users</h1>
+        <ul>
+          <li>
+            <span>Popular</span>
+          </li>
+          <li>
+            <span>Favorite</span>
+          </li>
+          <li>
+            <span>News</span>
+          </li>
+        </ul>
+      </header>
     </div>
-  )
-}
+  );
+};
 
 export default Users;
