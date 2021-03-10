4e8be691b45baf2596446eb22221dd3ed5f3769d
Avik Sargsyan <gukasyan.spa@gmail.com>
2021-03-10 15:23:45 +0400

    added improved router performance

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import App from "./App/App";
 import Courses from "./App/Components/Pages/Courses/Courses";
 import ShoppingList from "./App/Components/Pages/ShoppingList/ShoppingList";
@@ -10,14 +10,34 @@ import Users from "./App/Components/Pages/Users/Users";
 
 import "./index.css";
 
+const routeAttributes = [
+  {
+    path: "/",
+    component: Courses
+  },
+  {
+    path: "/shopping-list",
+    component: ShoppingList
+  },
+  {
+    path: "/users",
+    component: Users
+  }
+];
+
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <App>
         <Switch>
-          <Route exact path="/" component={Courses}/>
-          <Route path="/shopping-list" component={ShoppingList}/>
-          <Route path="/users" component={Users}/>
+          {routeAttributes.map((item, index) => (
+            <Route
+              exact
+              key={item.path + "-" + index}
+              path={item.path}
+              component={item.component}
+            />
+          ))}
         </Switch>
       </App>
     </Router>
