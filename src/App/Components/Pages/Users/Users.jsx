e472287dbc5919df7b31b26306c6748a1df4169f
Avik Sargsyan <gukasyan.spa@gmail.com>
2021-03-12 11:24:47 +0400

    fixed bugs and added improved "users" performance

--- src/App/Components/Pages/Users/Users.jsx
+++ src/App/Components/Pages/Users/Users.jsx
@@ -2,21 +2,32 @@ import React from "react";
 
 import "./Users.css";
 
+const nameOfSubtitles = [
+  {
+    id: 0,
+    name: "Popular"
+  },
+  {
+    id: 1,
+    name: "Favorite"
+  },
+  {
+    id: 2,
+    name: "News"
+  }
+];
+
 const Users = () => {
   return (
     <div className="users__container">
       <header>
         <h1>Users</h1>
         <ul>
-          <li>
-            <span>Popular</span>
-          </li>
-          <li>
-            <span>Favorite</span>
-          </li>
-          <li>
-            <span>News</span>
-          </li>
+          {nameOfSubtitles.map(item => (
+            <li key={`${item}-${item.id}`}>
+              <span>{item.name}</span>
+            </li>
+          ))}
         </ul>
       </header>
     </div>
